eab2c4fee1d47767548b208d289cf1cc1612b440
emtirabd <emtir1902@gmail.com>
2024-04-29 20:26:02 +0200

    correction account div

--- Frontend/src/components/account.jsx
+++ Frontend/src/components/account.jsx
@@ -5,18 +5,20 @@ function Account() {
     const user = data.usersAccounts.find((u) => u.id === "Tony Stark"); // Remplacez par l'utilisateur souhaité
 
   return (
-    <section className="account">
-      {user.accounts.map((account, index) => (
-        <div key={index} className="account-content-wrapper">
+    <div>
+    {user.accounts.map((account, index) => (
+      <section key={index} className="account">
+        <div className="account-content-wrapper">
           <h3 className="account-title">{account.name}</h3>
           <p className="account-amount">${account.solde}</p>
           <p className="account-amount-description">{account.balance}</p>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
         </div>
-      ))}
-    </section>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+    ))}
+  </div>
   )
 }
 export default Account
